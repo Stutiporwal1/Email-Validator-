08b46f104d4b3219813f619d8825f174aa5759f3
Stuti Porwal <porwalstuti01@gmail.com>
2025-07-20 07:51:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("This is my script")
+
+let result = {
+    "tag" : "",
+    "free" : false,
+    "role" : false,
+    "user" : "stutiporwal",
+    "email" : "stutiporwal1@gmail.com",
+    "score" : 0.64,
+    "state" : "undeliverable",
+    "domain" : "stutiporwal.com",
+    "reason" : "invalid_mailbox",
+    "mx_found" : true,
+    "catch_all" : null,
+    "disposable" : false,
+    "smtp_check" : false,
+    "did_you_mean" : "",
+    "format_valid" : true
+}
+
+submitBtn.addEventListener("click", async () => {
+    e.preventDefault()
+    console.log("Clicked!")
+    let email = document.getElementById("username").value
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ''
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key] !== " "){
+        str = str + '<div> ${key} : ${result[key]}</div>'
+        }
+    }
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
